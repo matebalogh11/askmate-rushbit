96869372744e59648e0e4d9e98c4ef6a1725111b
Balogh Máté <bmate11@bmate11>
2017-05-10 13:20:34 +0200

    data manager module was created

---
+++ data_manager.py
@@ -0,0 +1,31 @@
+import base64
+import csv
+
+# This module is for import only!
+
+
+def write_csv(file, data):
+
+    with open(file, "w", newline="") as text:
+        writer = csv.writer(text)
+        for i, items in enumerate(data):
+            for n, details in enumerate(items):
+                if n == 4 or n == 5 or i == 6:
+                    data[i][n] = base64.b64encode(bytes(data[i][n], "utf-8")).decode("utf-8")
+        for story in data:
+            writer.writerow(story)
+
+
+def read_csv(file):
+
+    with open(file, "r") as text:
+        reader = csv.reader(text)
+        requested_data = list(reader)
+        for n, data in enumerate(requested_data):
+            for i, items in enumerate(data):
+                if i == 1 or i == 2 or i == 3:
+                    requested_data[n][i] = int(requested_data[n][i])
+                if i == 4 or i == 5 or i == 6:
+                    requested_data[n][i] = base64.b64decode(requested_data[n][i]).decode("utf-8")
+
+    return requested_data
