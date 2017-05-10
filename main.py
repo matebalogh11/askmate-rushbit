1b3b66bbbd3a46f8353b16d85331a0dc9e4e22be
Annabella Malatinszki <a.malatinszki@gmail.com>
2017-05-10 15:33:57 +0200

    show_question_list function in main.py and list.html in working condition

--- main.py
+++ main.py
@@ -19,8 +19,9 @@ def exportdata(filename):
 # Routing
 @app.route("/")
 @app.route("/list")
-def show_questions():  # REQUIRED
-    pass
+def show_question_list():  # REQUIRED
+    questions = data_manager.read_csv("question.csv")
+    return render_template("list.html", questions=questions, title="Questions")
 
 
 @app.route("/new-question/")
@@ -30,7 +31,7 @@ def show_new_question_form():  # REQUIRED
 
 
 @app.route("/question/<question_id>")
-def show_single_question(question_id):  # REQUIRED
+def show_question_page(question_id):  # REQUIRED
     pass
 
 
@@ -63,7 +64,6 @@ def delete_answer(answer_id):
 @app.route("/question/<question-id>/vote-<direction>")
 def vote(direction):
     pass
-
 
 
 if __name__ == "__main__":
