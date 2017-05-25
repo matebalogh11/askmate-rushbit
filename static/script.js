0c425eead7002c46c329762d1f407cb268d7d43a
Balogh Máté <bmate11@bmate11>
2017-05-25 15:50:15 +0200

    edit option for answers was added to question.html and to the stylesheet

--- static/script.js
+++ static/script.js
@@ -7,13 +7,3 @@ function hideMe() {
         textbox.style.display = 'none';
     }
 }
-
-function hideMe_answer() {
-    var textbox = document.getElementById('a_comment');
-    var textboxstyle = window.getComputedStyle(textbox, null);
-    if (textboxstyle['display'] === 'none') {
-        textbox.style.display = 'block';
-    } else {
-        textbox.style.display = 'none';
-    }
-}
