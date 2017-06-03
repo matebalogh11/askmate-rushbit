343575aecc1ab30f5ff5a3eabb5599546ba1ec77
Mark Kovacs <kovacsmark89@gmail.com>
2017-06-03 22:21:00 +0200

    Detailed question page restructured HTML-CSS-wise and JavaScript functions fixed for buttons. New fixed buttons added to this page, for that 2 new images added.

--- static/script.js
+++ static/script.js
@@ -1,5 +1,6 @@
-function hideMe() {
-    var textbox = document.getElementById('q_comment');
+
+function hideMe_q_submit() {
+    var textbox = document.getElementById('q_comment_submit');
     var textboxstyle = window.getComputedStyle(textbox, null);
     if (textboxstyle['display'] === 'none') {
         textbox.style.display = 'block';
@@ -8,9 +9,30 @@ function hideMe() {
     }
 }
 
-function hideMe_answer() {
-    var textbox = document.getElementsByClassName('a_comment');
-    if (textbox.style.display === 'none') {
+function hideMe_q_edit(element_id) {
+    var textbox = document.getElementById(element_id);
+    var textboxstyle = window.getComputedStyle(textbox, null);
+    if (textboxstyle['display'] === 'none') {
+        textbox.style.display = 'block';
+    } else {
+        textbox.style.display = 'none';
+    }
+}
+
+function hideMe_a_submit(element_id) {
+    var textbox = document.getElementById(element_id);
+    var textboxstyle = window.getComputedStyle(textbox, null);
+    if (textboxstyle['display'] === 'none') {
+        textbox.style.display = 'block';
+    } else {
+        textbox.style.display = 'none';
+    }
+}
+
+function hideMe_a_edit(element_id) {
+    var textbox = document.getElementById(element_id);
+    var textboxstyle = window.getComputedStyle(textbox, null);
+    if (textboxstyle['display'] === 'none') {
         textbox.style.display = 'block';
     } else {
         textbox.style.display = 'none';
