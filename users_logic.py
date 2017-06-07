b173aa7628ac01f4732866bf05aa54d87b46faf3
Annabella Malatinszki <a.malatinszki@gmail.com>
2017-06-07 07:31:04 +0200

    Implement change_reputation_a to change reputation on votes on answers or accepted answers

--- users_logic.py
+++ users_logic.py
@@ -19,3 +19,25 @@ def change_reputation_q(question_id, direction):
     data2 = (amount, q_user_name)
     fetch2 = None
     db.run_statements(((SQL2, data2, fetch2),))
+
+
+# once the accpeted answer functions is written, this funtion should be called
+# within it with direction=None, acc=True
+def change_reputation_a(answer_id, direction=None, acc=None):
+    """Update user reputation if an answer of theirs is voted on or accepted."""
+    if acc:
+        amount = 15
+    elif direction == "up":
+        amount = 10votevote
+    elif direction == "down":
+        amount = -2
+    SQL1 = """SELECT user_name FROM answers WHERE answer_id = %s;"""
+    data1 = (answer_id,)
+    fetch1 = "one"
+    a_user_name = db.run_statements(((SQL1, data1, fetch1),))
+
+    SQL2 = """UPDATE users SET reputation = (reputation + %s)
+                WHERE users.user_name = %s;"""
+    data2 = (amount, a_user_name)
+    fetch2 = None
+    db.run_statements(((SQL2, data2, fetch2),))
